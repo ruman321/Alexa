0828cf0f65b1eae22e46c7ae5badecf99557ea74
ruman321 <ruman.mail421@gmail.com>
2024-10-26 22:04:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ou1mFDab#iwT9nvSKxVhxe_pBScBkDIzxE9SkkuNgtXDew46Mw6k",
+SESSION_ID: process.env.SESSION_ID || "R7EzjDJL#oXDCH-NDNVDkxbSyxXFPpx5JKQ-86-cAL5yjWbteY_k",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
